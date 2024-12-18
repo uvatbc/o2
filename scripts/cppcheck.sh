2187b1c5d0da783c0501c39b797a03b2a031a34b
Nyall Dawson <nyall.dawson@gmail.com>
2024-12-19 08:41:05 +1000

    Fix cppcheck

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -27,6 +27,7 @@ cppcheck --inline-suppr \
          -DQ_ENUMS = \
          -DQ_ENUM = \
          -DQ_DECLARE_OPERATORS_FOR_FLAGS = \
+         -DQ_SLOTS = \
          -Dforeach = \
          -j $(nproc) \
          ${SCRIPT_DIR}/../src \
